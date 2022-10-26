887161914a8e7445ec99133999acab365e6742fc
Bardia30 <110236333+Bardia30@users.noreply.github.com>
2022-10-26 07:35:00 -0400

    Add files via upload

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -13,40 +13,19 @@ for (let i=0; numberOfDrumButtons; i++){
 
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
         // this.style.color = 'white';
-        switch (this.innerHTML) {
-            case "w":
-                let tom1 = new Audio("sounds/tom-1.mp3");
-                tom1.play();
-                break;
-            case "a":
-                let tom2 = new Audio("sounds/tom-2.mp3");
-                tom2.play();
-            case "s":
-                let tom3 = new Audio("sounds/tom-3.mp3");
-                tom3.play();
-                break;
-            case "d":
-                let tom4 = new Audio("sounds/tom-4.mp3");
-                tom4.play();
-                break;
-            case "j":
-                let snare = new Audio("sounds/snare.mp3");
-                snare.play();
-                break;
-            case "k":
-                let kick = new Audio("sounds/kick-bass.mp3");
-                kick.play();
-                break;
-            case "l":
-                let crash = new Audio("sounds/crash.mp3");
-                crash.play();
-                break;
-            default:
-                break;
-        }
+        make_sound(this.innerHTML);
+        buttonAnimation(this.innerHTML);
         
     });
+    document.addEventListener("keypress",function (event){
+        make_sound(event.key);
+        buttonAnimation(event.key);
+    });
+
 };
+
+
+
 
 
 // let audio = new Audio("sounds/tom-1.mp3");
@@ -54,9 +33,45 @@ for (let i=0; numberOfDrumButtons; i++){
 
 
 
+function make_sound(key) {
+    switch (key) {
+        case "w":
+            let tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            let tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+        case "s":
+            let tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            let tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            let snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        case "k":
+            let kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+        case "l":
+            let crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        default:
+            break;
+    };
+};
 
-
-
+function buttonAnimation(currentKey) {
+    let activeButton = document.querySelector("."+ currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(function () {activeButton.classList.remove("pressed")},100);
+}
 
 
 
@@ -120,14 +135,19 @@ for (let i=0; numberOfDrumButtons; i++){
 
 //constructor function:
 
-function HouseKeeper (name, hasLingerie, age, specialKinks) {
-    this.name = name;
-    this.hasLingerie = hasLingerie;
-    this.age = age;
-    this.specialKinks = specialKinks;
-}
+// function HouseKeeper (name, hasLingerie, age, specialKinks) {
+//     this.name = name;
+//     this.hasLingerie = hasLingerie;
+//     this.age = age;
+//     this.specialKinks = specialKinks;
+//     this.suck = function () {
+//         console.log("ugh ugh guh");
+//         alert("hayaa");
+//     };
+// };
 
-// initialize object:
+// // initialize object:
 
-var houseKeeper1 = new HouseKeeper("Adriana",true,33,["anal","deep throat","squirt"]);
-var houseKeeper2 = new HouseKeeper("Angla",no,44,["Australian","huge boobs","Blue Eyes"]);
+// var houseKeeper1 = new HouseKeeper("Adriana",true,33,["anal","deep throat","squirt"]);
+// var houseKeeper2 = new HouseKeeper("Angla",no,44,["Australian","huge boobs","Blue Eyes"]);
+// houseKeeper1.suck();
